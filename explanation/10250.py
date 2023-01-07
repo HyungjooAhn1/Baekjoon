d750a040e5829e4deac5340d8851b05302a1d43b
Hyungjoo, Ahn <101160593+HyungjooAhn1@users.noreply.github.com>
2023-01-07 17:00:59 +0900

    Add files via upload

---
+++ explanation/10250.py
@@ -0,0 +1,7 @@
+num = int(input())
+for i in range(num):
+    h,w,n = map(int, input().split())
+    if n % h == 0:
+        print(100*h + (n // h))
+    else:
+        print(100*(n % h) + (n // h + 1))
