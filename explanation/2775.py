740157013faf72e54504c622d0b4392394be3ecd
Hyungjoo, Ahn <101160593+HyungjooAhn1@users.noreply.github.com>
2023-01-21 20:19:42 +0900

    Add files via upload

---
+++ explanation/2775.py
@@ -0,0 +1,9 @@
+count = int(input())
+for _ in range(count):
+    k = int(input())
+    n = int(input())
+    f0 = [i for i in range(1,n+1)]
+    for i in range(k):
+        for j in range(1,n):
+            f0[j] += f0[j-1]
+    print(f0[-1])
