77e97c82ee1ba5562f8f1242a2fbfa9be4134a71
Hyungjoo, Ahn <101160593+HyungjooAhn1@users.noreply.github.com>
2023-01-27 14:37:14 +0900

    Add files via upload

---
+++ explanation/1463.py
@@ -0,0 +1,12 @@
+# 동적 프로그래밍 (Dynamic Programming, DP)
+import sys
+input = sys.stdin.readline
+n = int(input())
+cnt = [0] * (n+1)
+for i in range(2,n+1):
+    cnt[i] = cnt[i-1] + 1
+    if i % 3 == 0:
+        cnt[i] = min(cnt[i], cnt[i//3] + 1)
+    if i % 2 == 0:
+        cnt[i] = min(cnt[i], cnt[i//2] + 1)
+print(cnt[n])
