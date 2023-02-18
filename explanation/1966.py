f4318be37cf4d38f5f4b930916162aa2c966bbe3
Hyungjoo, Ahn <101160593+HyungjooAhn1@users.noreply.github.com>
2023-02-18 22:55:28 +0900

    Add files via upload

---
+++ explanation/1966.py
@@ -0,0 +1,19 @@
+s = int(input())
+for _ in range(s):
+    n, m = map(int, input().split())
+    imp = list(map(int, input().split()))
+    idx = list(range(len(imp)))
+    idx[m] = 'target'
+    order = 0
+    while True:
+        if imp[0] == max(imp):
+            order += 1
+            if idx[0] == 'target':
+                print(order)
+                break
+            else:
+                imp.pop(0)
+                idx.pop(0)
+        else:
+            imp.append(imp.pop(0))
+            idx.append(idx.pop(0))
