3365b43c9fd8bf8eb80ea3b11742781f408156d0
Hyungjoo, Ahn <101160593+HyungjooAhn1@users.noreply.github.com>
2023-04-13 11:54:48 +0900

    Add files via upload

---
+++ explanation/11000.py
@@ -0,0 +1,15 @@
+import heapq
+n = int(input())
+time = []
+for _ in range(n):
+    time.append(list(map(int, input().split())))
+time = sorted(time, key=lambda x: x[0])
+room = []
+heapq.heappush(room, time[0][1])
+for i in range(1,n):
+    if time[i][0] < room[0]:
+        heapq.heappush(room, time[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, time[i][1])
+print(len(room))
